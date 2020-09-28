afcfdd368bf41585e945f68003579e305b1e58e9
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2020-09-28 19:51:48 +0530

    Added extension to URLSession to performSynchronous(url:) request.

--- DispatchSemaphore Example/ViewController.swift
+++ DispatchSemaphore Example/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+//    let operationQueue = OperationQueue()
+//    let dispatchGroup  = DispatchGroup()
     
     let pathOfImages = [
         "https://images.dog.ceo/breeds/papillon/n02086910_4505.jpg",
@@ -25,8 +27,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func syncLoadImagePressed(_ sender: Any) {
+        if let anImage = pathOfImages.randomElement(),
+           let url = URL(string: anImage){
+            imageView.image = syncFetchImage(fromUrl: url)
+        }
+    }
     
-    @IBAction func loadImagePressed(_ sender: UIButton) {
+    fileprivate func normalApiCallToLoadImage() {
         imageView.image = nil
         if let anImage = pathOfImages.randomElement(),
            let url = URL(string: anImage){
@@ -48,6 +56,10 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func loadImagePressed(_ sender: UIButton) {
+        normalApiCallToLoadImage()
+    }
     func requestImageFile(url: URL, completionHandler: @escaping (UIImage?, Error?) -> Void) {
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
@@ -66,6 +78,79 @@ class ViewController: UIViewController {
     }
     
     
+//    func fetchImage(fromUrl url: URL) -> UIImage? {
+//        print("fetchImage: Entered")
+//        var image: UIImage?
+//
+//        operationQueue.addOperation {
+//            self.dispatchGroup.enter()
+//            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//
+//                print("fetchImage: Received response")
+//                if let error = error {
+//                    print("fetchImage Error:\(error.localizedDescription)")
+//                }
+//                guard let data = data else {
+//                    print("fetchImage No data")
+//                    return
+//                }
+//
+//                image = UIImage(data: data)
+//                self.dispatchGroup.leave()
+//
+//            }
+//
+//            task.resume()
+//            _ = self.dispatchGroup.wait(timeout: .distantFuture)
+//        }
+//
+//        print("fetchImage: Returning")
+//        return image
+//    }
+    
+    func syncFetchImage(fromUrl url: URL) -> UIImage? {
+        var image: UIImage?
+        print("fetchImage: Starting")
+        
+        let (data, response, error) = URLSession.shared.performSynchronously(url: url)
+        print("fetchImage: Received response")
+        if let error = error {
+            print("fetchImage Error:\(error.localizedDescription)")
+        }
+        guard let imageData = data else {
+            print("fetchImage No data")
+            return nil
+        }
+        
+        image = UIImage(data: imageData)
+        print("fetchImage: returning image")
+        return image
+    }
+        
+    
 
 }
 
+
+extension URLSession {
+
+    func performSynchronously(url: URL) -> (data: Data?, response: URLResponse?, error: Error?) {
+        let semaphore = DispatchSemaphore(value: 0)
+
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+
+        let task = self.dataTask(with: url) {
+            data = $0
+            response = $1
+            error = $2
+            semaphore.signal()
+        }
+
+        task.resume()
+        semaphore.wait()
+
+        return (data, response, error)
+    }
+}
